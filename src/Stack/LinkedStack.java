9ea4b4b908a88c794fef1c98c4d7ee2317a62fec
foggel <foggel@gmail.com>
2018-12-19 00:40:07 +0100

    LinkedStack finnished

--- src/Stack/LinkedStack.java
+++ src/Stack/LinkedStack.java
@@ -4,27 +4,33 @@ public class LinkedStack<E> implements Stack<E> {
 
     private static class Node<E>{
 
-
+        private E elem;
+        private Node<E> next;
+        
+        private Node(E elem, Node<E> next){
+            this.elem = elem;
+            this.next = next;
+        }
 
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return top == null;
     }
 
     @Override
     public E top() {
-        return null;
+        return top.elem;
     }
 
     @Override
     public void pop() {
-
+        top = top.next;
     }
 
     @Override
     public void push(E e) {
-
+        top = new Node<>(e,top);
     }
 }
